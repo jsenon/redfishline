4d9d1e2f8e068d35334192a99d95bebcfd228e68
Julien Senon <julien.senon@gmail.com>
2017-06-20 19:52:30 +0200

    Build BIOS call API

--- src/web/web.go
+++ src/web/web.go
@@ -57,11 +57,28 @@ func Index(res http.ResponseWriter, req *http.Request) {
 
 func Send(res http.ResponseWriter, req *http.Request) {
 	// Retrieve info from form
+
 	req.ParseForm()
 
 	ILOHostname := req.FormValue("ILOHostname")
+	// Check if no Host to revent panic
+	if ILOHostname == "" {
+		fmt.Println("Error Hostname")
+		ILOHostname = "127.0.0.1"
+		// http.Error(res, "Error Hostname", 500)
+		http.Redirect(res, req, "/index", http.StatusSeeOther)
+		return
+	}
 	Username := req.FormValue("Username")
+	// Set Default Username if not provideed, prevent panic
+	if Username == "" {
+		Username = "Username"
+	}
+	// Set Default Password if not provideed, prevent panic
 	Password := req.FormValue("Password")
+	if Password == "" {
+		Password = "Password"
+	}
 	UEFI := req.FormValue("UEFI")
 	Legacy := req.FormValue("Legacy")
 	Useradd := req.FormValue("Useradd")
@@ -118,48 +135,20 @@ func Send(res http.ResponseWriter, req *http.Request) {
 		// Loop on Servers
 		for i := range Servers {
 
-			if UEFI == "on" {
-				fmt.Println("------> Launch MASSIVE API UEFI on", Servers[i].ILOHostname)
-
-			}
-			if Legacy == "on" {
-				fmt.Println("------> Launch MASSIVE API Legacy on", Servers[i].ILOHostname)
-			}
-			if Useradd == "on" {
-				fmt.Println("------> Launch MASSIVE API Useradd on", Servers[i].ILOHostname)
-			}
-			if PowerHigh == "on" {
-				fmt.Println("------> Launch MASSIVE API PowerHigh on", Servers[i].ILOHostname)
-			}
-			if FastBoot == "on" {
-				fmt.Println("------> Launch MASSIVE API FastBoot on", Servers[i].ILOHostname)
-			}
-		}
-
-	} else {
-
-		// Call to API ILO x-auth-token
-
-		// Execute Differente API Call for ILO Configuration
-		if UEFI == "on" {
-			fmt.Println("------> Launch API UEFI")
-		}
-		if Legacy == "on" {
-			fmt.Println("------> Launch API Legacy")
-			url := "https://" + ILOHostname + "/redfish/v1/SessionService/Sessions/"
+			url := "https://" + Servers[i].ILOHostname + "/redfish/v1/SessionService/Sessions/"
 			fmt.Println("URL:>", url)
-			jsonStr := Credential{Username, Password}
+			// Retrieve X-Auth-Token
+			// Create my Body
+			jsonStr := Credential{Servers[i].Username, Servers[i].Password}
 			theJson, _ := json.Marshal(jsonStr)
-
-			// var jsonStr = []byte(`{"UserName": Username,"Password": Password}`)
 			fmt.Println("Body:>", jsonStr)
+
 			// Disable self certificate check
 			tr := &http.Transport{
 				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 			}
 
 			req, err := http.NewRequest("POST", url, bytes.NewBuffer(theJson))
-
 			// req.Header.Set("X-Custom-Header", "")
 			req.Header.Set("Content-Type", "application/json")
 
@@ -177,17 +166,173 @@ func Send(res http.ResponseWriter, req *http.Request) {
 			fmt.Println("response Body:", string(body))
 
 			// Retrieve x-auth-token
-			//token := resp.Header.Get("x-auth-token")
+			token := resp.Header.Get("x-auth-token")
+			fmt.Println(token)
+
+			// url2 := "https://" + Servers[i].ILOHostname + "/redfish/v1/Systems/1/Bios/Settings/"
+			// jsonStr2 := []byte(`{"BootMode":""}`)
+			// req2, err2 := http.NewRequest("PATCH", url2, bytes.NewBuffer(jsonStr2))
+			// client2 := &http.Client{Transport: tr}
+
+			if Legacy == "on" && UEFI == "on" {
+				fmt.Println("Error Legacy and UEFI in the same time")
+			}
+
+			if UEFI == "on" && Legacy == "" {
+				fmt.Println("------> Launch MASSIVE API UEFI on", Servers[i].ILOHostname)
+
+			}
+			if Legacy == "on" && UEFI == "" {
+				fmt.Println("------> Launch MASSIVE API Legacy on", Servers[i].ILOHostname)
+			}
+			if Useradd == "on" {
+				fmt.Println("------> Launch MASSIVE API Useradd on", Servers[i].ILOHostname)
+			}
+			if PowerHigh == "on" {
+				fmt.Println("------> Launch MASSIVE API PowerHigh on", Servers[i].ILOHostname)
+			}
+			if FastBoot == "on" {
+				fmt.Println("------> Launch MASSIVE API FastBoot on", Servers[i].ILOHostname)
+			}
+		}
+
+	} else {
+
+		// Call to API ILO x-auth-token
+
+		url := "https://" + ILOHostname + "/redfish/v1/SessionService/Sessions/"
+		fmt.Println("URL:>", url)
+		// Retrieve X-Auth-Token
+		// Create my Body
+		jsonStr := Credential{Username, Password}
+		theJson, _ := json.Marshal(jsonStr)
+		fmt.Println("Body:>", jsonStr)
+
+		// Disable self certificate check
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(theJson))
+		// req.Header.Set("X-Custom-Header", "")
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{Transport: tr}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+		fmt.Println("AUTH:", resp.Header.Get("x-auth-token"))
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(body))
+
+		// Retrieve x-auth-token
+		token := resp.Header.Get("x-auth-token")
+
+		url2 := "https://" + ILOHostname + "/redfish/v1/Systems/1/Bios/Settings/"
+		jsonStr2 := []byte(`{"BootMode":""}`)
+		req2, err2 := http.NewRequest("PATCH", url2, bytes.NewBuffer(jsonStr2))
+		client2 := &http.Client{Transport: tr}
+
+		// Execute Differente API Call for ILO Configuration
+
+		// Prevent UEFI and Legacy BIOS in the same time
+		if Legacy == "on" && UEFI == "on" {
+			fmt.Println("Error Legacy and UEFI in the same time")
+		}
+
+		if UEFI == "on" && Legacy == "" {
+			fmt.Println("------> Launch API UEFI")
+			// Send BIOS = UEFI
+			jsonStr2 = []byte(`{"BootMode":"UEFI"}`)
+			req2, err2 = http.NewRequest("PATCH", url2, bytes.NewBuffer(jsonStr2))
+			if err2 != nil {
+				panic(err2)
+			}
+			req2.Header.Set("X-Auth-Token", token)
+			req2.Header.Set("Content-Type", "application/json")
+			fmt.Println("URL:>", url2)
+			resp2, err2 := client2.Do(req2)
+			if err2 != nil {
+				panic(err2)
+			}
+			body2, _ := ioutil.ReadAll(resp2.Body)
+			fmt.Println("response Status:", resp2.Status)
+			fmt.Println("response Headers:", resp2.Header)
+			fmt.Println("response Body:", string(body2))
+		}
+		if Legacy == "on" && UEFI == "" {
+			fmt.Println("------> Launch API Legacy")
+
+			// Send BIOS = Legacy
+			jsonStr2 = []byte(`{"BootMode":"LegacyBios"}`)
+			req2, err2 = http.NewRequest("PATCH", url2, bytes.NewBuffer(jsonStr2))
+			if err2 != nil {
+				panic(err2)
+			}
+			req2.Header.Set("X-Auth-Token", token)
+			req2.Header.Set("Content-Type", "application/json")
+			fmt.Println("URL:>", url2)
+			resp2, err2 := client2.Do(req2)
+			if err2 != nil {
+				panic(err2)
+			}
+			body3, _ := ioutil.ReadAll(resp2.Body)
+			fmt.Println("response Status:", resp2.Status)
+			fmt.Println("response Headers:", resp2.Header)
+			fmt.Println("response Body:", string(body3))
 
 		}
+
 		if Useradd == "on" {
 			fmt.Println("------> Launch API Useradd")
+			// Need to unscope to Administrator and retrieve token instead of openstack user
+
 		}
 		if PowerHigh == "on" {
 			fmt.Println("------> Launch API PowerHigh")
+			// Send BIOS = MaxPerf
+			jsonStr2 = []byte(`{"PowerProfile":"MaxPerf"}`)
+			req2, err2 = http.NewRequest("PATCH", url2, bytes.NewBuffer(jsonStr2))
+			if err2 != nil {
+				panic(err2)
+			}
+			req2.Header.Set("X-Auth-Token", token)
+			req2.Header.Set("Content-Type", "application/json")
+			fmt.Println("URL:>", url2)
+			resp2, err2 := client2.Do(req2)
+			if err2 != nil {
+				panic(err2)
+			}
+			body4, _ := ioutil.ReadAll(resp2.Body)
+			fmt.Println("response Status:", resp2.Status)
+			fmt.Println("response Headers:", resp2.Header)
+			fmt.Println("response Body:", string(body4))
+
 		}
 		if FastBoot == "on" {
 			fmt.Println("------> Launch API FastBoot")
+			// Send BIOS = Extended Memory Test Off
+			jsonStr2 = []byte(`{"ExtendedMemTest":"Disabled"}`)
+			req2, err2 = http.NewRequest("PATCH", url2, bytes.NewBuffer(jsonStr2))
+			if err2 != nil {
+				panic(err2)
+			}
+			req2.Header.Set("X-Auth-Token", token)
+			req2.Header.Set("Content-Type", "application/json")
+			fmt.Println("URL:>", url2)
+			resp2, err2 := client2.Do(req2)
+			if err2 != nil {
+				panic(err2)
+			}
+			body5, _ := ioutil.ReadAll(resp2.Body)
+			fmt.Println("response Status:", resp2.Status)
+			fmt.Println("response Headers:", resp2.Header)
+			fmt.Println("response Body:", string(body5))
 		}
 
 	}
@@ -195,8 +340,8 @@ func Send(res http.ResponseWriter, req *http.Request) {
 	// Execute Power Setting action
 
 	// Get Information
-	// Check if we launch at each reload or laumch on demand
-	fmt.Println("------> Launch API Information")
+	// Check if we launch at each reload or launch on demand
+	//fmt.Println("------> Launch API Information")
 
 	// http redirect to index
 	http.Redirect(res, req, "/index", http.StatusSeeOther)
@@ -222,9 +367,19 @@ func Inventory(res http.ResponseWriter, req *http.Request) {
 }
 
 func Rebootquick(res http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
 	t, _ := template.ParseFiles("templates/Reboot.html")
 	t.Execute(res, Server)
 	fmt.Println("------> Launch API RebootQuick")
+	req.ParseForm()
+
+	ILOHostname := req.FormValue("ILOHostname")
+	Username := req.FormValue("Username")
+	Password := req.FormValue("Password")
+
+	fmt.Println("ILO", ILOHostname)
+	fmt.Println("User", Username)
+	fmt.Println("Password", Password)
 
 }
 
